5980ce807f3ea8a618fac8e2f815b69e2bda8e2c
MikeReCode <mihail.radu.work@gmail.com>
2023-12-28 15:56:38 +0200

    Add functionality to create ingredients from recipe API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -11,7 +11,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
 from recipe.serializers import (
     RecipeSerializer,
@@ -286,3 +286,50 @@ class PrivateRecipeAPITest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.tags.count(), 0)
+
+    def test_create_recipe_with_new_ingredients(self):
+        """Test create a new recipe with new ingredient"""
+        payload = {
+            "title": "Sample recipe title API",
+            'time_minutes': 25,
+            'price': Decimal('5.15'),
+            'description': 'Sample description API',
+            'link': 'http://example.com/recipeAPI.pdf',
+            'ingredients': [{'name': "Cauliflower"}, {'name': 'Salt'}],
+        }
+
+        res = self.client.post(RECIPES_URL, payload, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipes = Recipe.objects.filter(user=self.user)
+        self.assertEqual(recipes.count(), 1)
+        recipe = recipes[0]
+        self.assertEqual(recipe.ingredients.count(), 2)
+        for ingredient in payload['ingredients']:
+            exists = recipe.ingredients.filter(name=ingredient['name'],
+                                        user=self.user).exists()
+            self.assertTrue(exists)
+
+    def test_create_recipe_with_existing_ingredients(self):
+            """Test create a new recipe with existing ingredient"""
+            payload = {
+                "title": "Sample recipe title API",
+                'time_minutes': 25,
+                'price': Decimal('5.15'),
+                'description': 'Sample description API',
+                'link': 'http://example.com/recipeAPI.pdf',
+                'ingredients': [{'name': "Cauliflower"}, {'name': 'Bread'}],
+            }
+            bread = Ingredient.objects.create(user=self.user, name='Bread')
+            res = self.client.post(RECIPES_URL, payload, format='json')
+            
+            self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+            recipes = Recipe.objects.filter(user=self.user)
+            self.assertEqual(recipes.count(), 1)
+            recipe = recipes[0]
+            self.assertEqual(recipe.ingredients.count(), 2)
+            self.assertIn(bread, recipe.ingredients.all())
+            for ingredient in payload['ingredients']:
+                exists = recipe.ingredients.filter(name=ingredient['name'],
+                                            user=self.user).exists()
+                self.assertTrue(exists)
